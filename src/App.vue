b6593fb13cc208224deff83b32d47b600f77f07c
fanx <708931190@qq.com>
2021-01-05 20:31:02 +0800

    vue-router的使用

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,48 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <!--    方式一：通过路由组件实现路由-->
+        <router-link to="/home">首页</router-link>
+        <router-link to="/about">关于</router-link>
+
+
+        <!--    方式二：通过html标签并绑定函数实现路由-->
+        <button @click="firstPageClick()">首页</button>
+        <button @click="aboutPageClick()">关于</button>
+        <button @click="userPageClick()">用户</button>
+        <router-link tag="button" :to="{name:'user',params:{userId:123}}">用户ID</router-link>
+        <router-link tag="button" :to="{path:'/query',query:{name:'xiaoming',age:'18'}}">用户信息</router-link>
+        <button @click="userPageClick()">用户ID</button>
+        <button @click="userGetParamsClick()">query传参</button>
+        <router-view/>
     </div>
-    <router-view/>
-  </div>
 </template>
 
+<script>
+    export default {
+        name: 'App',
+        data() {
+            return {
+                userId: 'zhangsan'
+            }
+        },
+        methods: {
+            firstPageClick() {
+                this.$router.push("/home");
+            },
+            aboutPageClick() {
+                this.$router.push("/about");
+            },
+            userPageClick() {
+                this.$router.push("/user/" + this.userId);
+            },
+            userGetParamsClick() {
+                this.$router.push({
+                    path: '/query',
+                    query: {name: 'xiaohong', age: '20'}
+                });
+            }
+        }
+    }
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,85 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+// import Home from "../components/Home";
+// import About from "../components/About";
+// import User from "../components/User";
 
-Vue.use(VueRouter)
+// 懒加载导包方式
+const Home = () => import('../components/Home');
+const About = () => import('../components/About');
+const User = () => import('../components/User');
+const HomeNews = () => import('../components/HomeNews');
+const HomeMessage = () => import('../components/HomeMessage');
+const Query = () => import('../components/Query');
 
-const routes = [
-  {
+
+// 解决vue-router在3.0版本以上重复点菜单报错问题
+const originalPush = VueRouter.prototype.push;
+VueRouter.prototype.push = function push(location) {
+    return originalPush.call(this, location).catch(err => err)
+};
+
+// 1.注册vue_router组件
+Vue.use(VueRouter);
+
+// 2.创建vue_router实例
+const routes = [{
     path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
+    redirect: '/home'
+}, {
+    path: '/home',
+    component: Home,
+    children: [{
+        path: '/',
+        redirect: 'news'
+    }, {
+        path: 'news',
+        component: HomeNews,
+        meta: {
+            title: '新闻'
+        }
+    }, {
+        path: 'message',
+        component: HomeMessage,
+        meta: {
+            title: '消息'
+        }
+    }],
+    meta: {
+        title: '首页'
+    }
+}, {
     path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
+    component: About,
+    meta: {
+        title: '关于'
+    }
+}, {
+    path: '/user/:userId',
+    component: User,
+    name: 'user',
+    meta: {
+        title: '用户'
+    }
+}, {
+    path: '/query',
+    component: Query,
+    name:'query',
+    meta: {
+        title: '查询'
+    }
+}];
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
+    routes: routes,
+    mode: 'history'
+});
 
+//路由导航
+router.beforeEach((to, from, next) => {
+    console.log(to);
+    document.title = to.matched[0].meta.title;
+    next();
+});
+
+//3.导出vue_router
 export default router
